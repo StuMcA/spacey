494674429b96c0a56fd698f678a3cac57f3d8e09
StuMcA <stuart.mcara@outlook.com>
2021-04-16 22:02:09 +0100

    added planets

--- client/src/components/SolarSystemContainer.vue
+++ client/src/components/SolarSystemContainer.vue
@@ -2,15 +2,15 @@
     <section>
         <button>Change view</button>
         <section id="solar-system">
-            <div id="sun"></div>
-            <div id="mercury"></div>
-            <div id="venus"></div>
-            <div id="earth"></div>
-            <div id="mars"></div>
-            <div id="jupiter"></div>
-            <div id="saturn"></div>
-            <div id="uranus"></div>
-            <div id="neptune"></div>
+            <div id="sun">Sun</div>
+            <div id="mercury" class="planet">M</div>
+            <div id="venus" class="planet">V</div>
+            <div id="earth" class="planet">E</div>
+            <div id="mars" class="planet">M</div>
+            <div id="jupiter" class="planet">J</div>
+            <div id="saturn" class="planet">S</div>
+            <div id="uranus" class="planet">U</div>
+            <div id="neptune" class="planet">N</div>
         </section>
     </section>
 </template>
@@ -22,6 +22,21 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
+button {
+    margin: 20px auto 20px 20px;
+}
+
+.planet {
+    width: 50px;
+    height: 50px;
+    border: 1px solid black;
+    border-radius: 50%;
+    position: absolute;
+    text-align: center;
+}
+
+
 
 </style>
