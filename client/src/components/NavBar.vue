95b63b1dff08a00796e9b8380619f7ff0e6e6d2d
StuMcA <stuart.mcara@outlook.com>
2021-04-16 19:21:59 +0100

    added planets drop down

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,8 +1,16 @@
 <template>
     <ul>
-        <li>Home</li>
-        <li>Planets</li>
-        <li>Moons</li>
+        <li>
+            <p>Home</p>
+        </li>
+        <li id="nav-bar-planet">
+            <p>Planets</p>
+            <planet-list id="planet-list"/>
+        </li>
+        <li>
+            <p>Moons</p>
+
+        </li>
         <li id="search">
             <label for="search-text">&#x1F50D;</label>
             <input type="text" id="search-text" placeholder="Search here">
@@ -11,11 +19,12 @@
 </template>
 
 <script>
-
+import PlanetList from '../components/PlanetList.vue'
 
 export default {
+    name: "nav-bar",
     components: {
-
+        "planet-list": PlanetList
     }
 }
 </script>
@@ -32,15 +41,39 @@ ul {
 
 li {
     display: flex;
-    justify-content: center;
     list-style: none;
     height: 30px;
     width: 160px;
-    align-items: center;
+    /* border: 1px solid black; */
+}
+
+p {
+    margin: 4px auto;
 }
 
 li:hover, label:hover {
     cursor: pointer;
+}
+
+li:hover {
+    background-color: rgba(0, 0, 0, 0.149);
+}
+
+#nav-bar-planet {
+    display: flex;
+    flex-direction: column;
+}
+
+#planet-list {
+    display: none;
+    border: none;
+}
+
+
+#nav-bar-planet:hover #planet-list {
+    display: block;
+    border: none;
+
 }
 
 #search {
---
+++ client/src/components/PlanetList.vue
@@ -0,0 +1,41 @@
+<template>
+  <ul>
+    <li v-for="(planet, index) in planets" :key="index">
+        <p>{{planet}}</p>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+
+data() {
+    return {
+        planets: [
+            "Mercury",
+            "Venus",
+            "Earth",
+            "to",
+            "be",
+            "replaced",
+            "with",
+            "api data"
+        ]
+    }
+}
+
+}
+</script>
+
+<style scoped>
+
+li {
+    list-style: none;
+    margin-left: -40px;
+}
+
+li:hover {
+    background: rgba(0, 0, 0, 0.067)
+}
+
+</style>
