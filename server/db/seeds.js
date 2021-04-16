240c293ca90178738e3b881387c3779afca278f6
SinnottTM <tim.sinnott86@gmail.com>
2021-04-16 15:46:08 +0100

    seeds Venus added

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -33,9 +33,47 @@ db.planets.insertMany([
             thirteenth: "Mercury does not experience any seasons.The axis of Mercury has the smallest tilt of all other planets, and this results in a lack of seasons on its surface.",
             fourteenth: "Mercury is the only planet which doesn’t rotate exactly once every year – instead rotating three times for every two orbits of the Sun.This is because it is nearly tidally locked to the Sun.",
             fifteenth: "The orbit of Mercury was important in proving Albert Einstein’s theory of General Relativity.",
-
         },
-    
+
     },
+
+    {
+
+        name: "Venus",
+        position_from_sun: 2,
+        latin_name: "Venus",
+        sumerian_name: "Inanna",
+        orbit_distance_km: 108209475,
+        orbit_distance_au: 0.732,
+        equatorial_diameter_km: 12104,
+        mass: "4.87 × 10^24 kg (0.82 Earths)",
+        number_of_moons: 0,
+        moon_names: {
+            first: null,
+        },
+        orbit_distance_km: 108209475,
+        orbit_distance_au: 0.73,
+        orbit_period_days: 225,
+        surface_temperature_max_celcius: 462,
+        surface_temperature_min_celcius: "see max",
+        facts: {
+            first: "Venus is the second brightest natural object in the sky. The planet has an apparent magnitude of -3.8 to -4.6, which makes it visible on a bright, clear day. The Moon is the only other natural object that is brighter.",
+            second: "Venus is sometimes referred to as the “morning star” and “evening star”.This dates back to ancient civilizations who believed that Venus was in fact two distinct stars appearing in the sky.When the orbit of Venus overtakes Earth’s orbit, it changes from being visible at sunrise to being visible at sunset.They were known as Phosphorus and Hesperus by the Greeks, and Lucifer and Vesper by the Romans.",
+            third: "One day on Venus is longer than one year. Due to the slow rotation on its axis, it takes 243 Earth-days to complete one rotation. The orbit of the planet takes 225 Earth-days – making a year on Venus shorter on day on Venus.",
+            fifth: "Venus is named after the Roman goddess of love and beauty. This may be, in part, due the brightness of the planet and may date back to the Babylonians in 1581 who referred to Venus as 'bright queen of the sky'.",
+            sixth: "Venus is sometimes called Earth’s sister planet. This is because their size is very similar (there is only a 638 km different in diameter) and Venus has around 81% of Earth’s mass. They are also similarly located with Venus being the closest planet to Earth. Both planets also have a central core, a molten mantle and a crust.",
+            seventh: "Venus has no moons nor rings.",
+            eighth: "Billions of years ago, the climate of Venus may been similar to that of Earth and scientists believe that Venus once possessed large amounts of water or oceans. However, due to the high temperatures produce from the extreme greenhouse effect, this water boiled off long ago and the surface of the planet is now too hot and hostile to sustain life.",
+            ninth: "Venus rotate in the opposite direction to other planets. Most other planets rotate counter-clockwise on their axis, however Venus, like Uranus, rotates clockwise. This is known as a retrograde rotation and may have been caused by a collision with an asteroid or other object which caused the planet to change its rotational path.",
+            tenth: "Venus is the hottest planet in the solar system with an average surface temperature of 462°C (863°F). Also, Venus doesn’t tilt on its axis which means there are no seasons either. The atmosphere is a dense 96.5% carbon dioxide which traps heat and caused the greenhouse effect which evaporated any water sources billions of years ago.",
+            eleventh: "The temperature on Venus doesn’t vary much between the night and day. This is due to the slow movement of the solar winds across the surface of the planet.",
+            twelfth: "The estimated age of the Venusian surface is around 300-400 million years old. By comparison, the surface of the Earth is about 100 million years old.",
+            thirteenth: "The atmospheric pressure of Venus is 92 times stronger than Earth’s. This means that any small asteroids entering the atmosphere of Venus are crushed by the immense pressure, which is why there are no small surface craters on the planet. This pressure is equivalent to being around 1,000 km under Earth’s oceans.",
+            fourteenth: "Venus has a very weak magnetic field. This surprised scientists, who expected Venus to have a magnetic field similar in strength to Earth’s. One possible reason for this is that Venus has no solid inner core, or that its core is not cooling.",
+            fifteenth: "Venus is the only planet in the Solar System to be named after a female figure.",
+        },
+    },
+
+    
 
 ]);
